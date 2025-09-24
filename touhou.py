318ae4941fb399ef973fdafcebbcf7cded6c9dab
Jesse Foster <jessc788@yahoo.com>
2025-09-23 19:55:11 -0500

    Create touhou.py

---
+++ touhou.py
@@ -0,0 +1,66 @@
+import random
+
+_2hus = ['Reimu', 'Marisa', 'Sanae', 'Suwako', 'Kanako', 'Yukari', 
+	 'Ran', 'Chen', 'Yuyuko', 'Youmu', 'Satori', 'Koishi', 'Orin', 
+	 'Okuu', 'Cirno', 'Daiyousei', 'Rumia', 'Meiling', 'Koakuma', 
+	 'Patchouli', 'Sakuya', 'Remilia', 'Flandre', 'Parsee', 
+	 'Yuuka', 'Resin', 'Tewi', 'Mokou', 'Kaguya', 'Eirin', 
+	 'Keine', 'Mystia', 'Aya', 'Momiji', 'Futo', 'Iku', 'Tenshi', 
+	 'Doremy', 'Yorihime', 'Toyohime', 'Nue', 'Kogasa', 'Alice', 
+	 'Junko', 'Hecatia', 'Clownpiece', 'Okina', 'Wriggle', 
+	 'Nareko', 'Aunn', 'Nitori', 'Hina', 'Eiki', 'Kasen', 'Seija', 
+	 'Byakuren', 'Nazrin', 'Sekibanki', 'Kokoro', 'Yuugi', 
+	 'Suika', 'Lily', 'Letty', 'Sunny', 'Luna', 'Star', 'Satono', 
+	 'Mai', 'Mamizou', 'Minamitsu', 'Ichirin', 'Mike', 'Kisume', 
+	 'Yamame', 'Komachi', 'Kutaka', 'Mima', 'Miko']
+
+def play():
+	random_2hu = random.choice(_2hus)
+	guess_count = 1
+	hints_given = 0
+	guess = input('Guess the 2hu I\'m thinking of: ')
+	
+	while guess.lower() != random_2hu.lower():
+		guess = input('Nope. Try again: ')
+		guess_count+=1
+		
+		if guess_count % 5 == 0 and hints_given < 3:
+			hint_ans = input('Do you want a hint? (y/n)')
+			
+			while True:
+				if hint_ans.lower() == 'y':
+					hints_given+=1
+					
+					if hints_given == 1:
+						print(f'The name is {len(random_2hu)} letters long.')
+						guess = input('Guess the 2hu I\'m thinking of: ')
+						guess_count+=1
+						break
+					elif hints_given == 2:
+						print(f'The first letter is {random_2hu[0].lower()}.')
+						guess = input('Guess the 2hu I\'m thinking of: ')
+						guess_count+=1
+						break
+					elif hints_given == 3:
+						print(f'The last letter is {random_2hu[-1].lower()}. (This is the last hint.)')
+						guess = input('Guess the 2hu I\'m thinking of: ')
+						guess_count+=1
+						break
+				elif hint_ans.lower() == 'n':
+					print('Suit yourself...')
+					break
+				else:
+					hint_ans = input('No breaking this program on my watch. Try again. (y/n ONLY)')
+	
+	print(f'Nice! I was thinking of {random_2hu}!')
+
+ans = input('Would you like to guess the 2hu? (y/n)')
+while True:
+	if ans.lower() == 'y':
+		play()
+		ans = input('Would you like to play again? (y/n)')
+	elif ans.lower() == 'n':
+		print('I see how it is... Then leave...')
+		break
+	else:
+		ans = input('No breaking this program on my watch. Try again. (y/n ONLY)')
